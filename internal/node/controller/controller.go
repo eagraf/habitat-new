bace6f6c31533dacd3e425aaa21b1c97b9db7e33
ethan <graf.ethan@gmail.com>
2024-01-28 01:42:51 -0500

    Split out user controls

--- internal/node/controller/controller.go
+++ internal/node/controller/controller.go
@@ -1,11 +1,9 @@
 package controller
 
 import (
-	"encoding/json"
 	"fmt"
 
 	"github.com/eagraf/habitat-new/internal/node/habitat_db"
-	"github.com/eagraf/habitat-new/internal/node/habitat_db/state"
 	"github.com/eagraf/habitat-new/internal/node/habitat_db/state/schemas/node"
 	"github.com/google/uuid"
 	"github.com/rs/zerolog/log"
@@ -29,47 +27,6 @@ func (c *NodeController) InitializeNodeDB() error {
 	}
 
 	return nil
-}
-
-func (c *NodeController) AddUser(userID, username, certificate string) error {
-	db, err := c.databaseManager.GetDatabaseByName(NodeDBDefaultName)
-	if err != nil {
-		return err
-	}
-
-	_, err = db.Controller.ProposeTransitions([]state.Transition{
-		&node.AddUserTransition{
-			UserID:      userID,
-			Username:    username,
-			Certificate: certificate,
-		},
-	})
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (c *NodeController) GetUserByUsername(username string) (*node.User, error) {
-	db, err := c.databaseManager.GetDatabaseByName(NodeDBDefaultName)
-	if err != nil {
-		return nil, err
-	}
-
-	var nodeState node.NodeState
-	err = json.Unmarshal(db.Controller.Bytes(), &nodeState)
-	if err != nil {
-		return nil, err
-	}
-
-	for _, user := range nodeState.Users {
-		if user.Username == username {
-			return user, err
-		}
-	}
-
-	return nil, fmt.Errorf("user with username %s not found", username)
 }
 
 // TODO this is basically a placeholder until we actually have a way of generating
