588ecf7758bd8544178e4f2b86c593f9aa1b266e
ethan <graf.ethan@gmail.com>
2024-02-04 11:05:42 -0500

    Move controller handlers to be next to controller methods for readability

--- internal/node/controller/apps.go
+++ internal/node/controller/apps.go
@@ -1,9 +1,62 @@
 package controller
 
 import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/eagraf/habitat-new/api/types"
 	"github.com/eagraf/habitat-new/internal/node/habitat_db/state"
 	"github.com/eagraf/habitat-new/internal/node/habitat_db/state/schemas/node"
+	"github.com/gorilla/mux"
 )
+
+// Handlers
+
+type PostAppHandler struct {
+	nodeController NodeController
+}
+
+func NewPostAppHandler(nodeController NodeController) *PostAppHandler {
+	return &PostAppHandler{
+		nodeController: nodeController,
+	}
+}
+
+func (h *PostAppHandler) Pattern() string {
+	return "/node/users/{user_id}/apps"
+}
+
+func (h *PostAppHandler) Method() string {
+	return http.MethodPost
+}
+
+func (h *PostAppHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "invalid method, require POST", http.StatusMethodNotAllowed)
+		return
+	}
+
+	vars := mux.Vars(r)
+	userID := vars["user_id"]
+
+	var req types.PostAppRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = h.nodeController.InstallApp(userID, req.AppInstallation)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// TODO validate request
+	w.WriteHeader(http.StatusCreated)
+}
+
+// Controller methods
 
 func (c *BaseNodeController) InstallApp(userID string, newApp *node.AppInstallation) error {
 	db, err := c.databaseManager.GetDatabaseByName(NodeDBDefaultName)
--- internal/node/controller/user.go
+++ internal/node/controller/user.go
@@ -3,10 +3,111 @@ package controller
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
 
+	"github.com/eagraf/habitat-new/api/types"
+	"github.com/eagraf/habitat-new/internal/node/habitat_db"
 	"github.com/eagraf/habitat-new/internal/node/habitat_db/state"
 	"github.com/eagraf/habitat-new/internal/node/habitat_db/state/schemas/node"
 )
+
+// Handlers
+
+type GetNodeHandler struct {
+	dbManager *habitat_db.DatabaseManager
+}
+
+func NewGetNodeHandler(dbManager *habitat_db.DatabaseManager) *GetNodeHandler {
+	return &GetNodeHandler{
+		dbManager: dbManager,
+	}
+}
+
+func (h *GetNodeHandler) Pattern() string {
+	return "/node"
+}
+
+func (h *GetNodeHandler) Method() string {
+	return http.MethodGet
+}
+
+func (h *GetNodeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	db, err := h.dbManager.GetDatabaseByName(NodeDBDefaultName)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	stateBytes, err := db.Bytes()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var stateMap map[string]interface{}
+	err = json.Unmarshal(stateBytes, &stateMap)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	resp := types.GetDatabaseResponse{
+		DatabaseID: db.ID,
+		State:      stateMap,
+	}
+
+	respBody, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(respBody)
+}
+
+type PostUserHandler struct {
+	nodeController NodeController
+}
+
+func NewPostUserHandler(nodeController NodeController) *PostUserHandler {
+	return &PostUserHandler{
+		nodeController: nodeController,
+	}
+}
+
+func (h *PostUserHandler) Pattern() string {
+	return "/node/users"
+}
+
+func (h *PostUserHandler) Method() string {
+	return http.MethodPost
+}
+
+func (h *PostUserHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "invalid method, require POST", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var req types.PostUserRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = h.nodeController.AddUser(req.UserID, req.Username, req.Certificate)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// TODO validate request
+	w.WriteHeader(http.StatusCreated)
+}
+
+// Controller methods
 
 func (c *BaseNodeController) AddUser(userID, username, certificate string) error {
 	db, err := c.databaseManager.GetDatabaseByName(NodeDBDefaultName)
