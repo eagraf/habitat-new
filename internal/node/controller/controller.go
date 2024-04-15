c777d4ee5aeb45bdc53295a52abb89a64d867a69
ethan <graf.ethan@gmail.com>
2024-04-14 22:08:28 -0400

    Controller generates init state based off of empty state migrations

--- internal/node/controller/controller.go
+++ internal/node/controller/controller.go
@@ -39,7 +39,12 @@ type BaseNodeController struct {
 
 // InitializeNodeDB tries initializing the database; it is a noop if a database with the same name already exists
 func (c *BaseNodeController) InitializeNodeDB() error {
-	_, err := c.databaseManager.CreateDatabase(constants.NodeDBDefaultName, node.SchemaName, generateInitState(c.nodeConfig))
+	initState, err := generateInitState(c.nodeConfig)
+	if err != nil {
+		return err
+	}
+
+	_, err = c.databaseManager.CreateDatabase(constants.NodeDBDefaultName, node.SchemaName, initState)
 	if err != nil {
 		if _, ok := err.(*hdb.DatabaseAlreadyExistsError); ok {
 			log.Info().Msg("Node database already exists, doing nothing.")
@@ -252,22 +257,22 @@ func (c *BaseNodeController) GetUserByUsername(username string) (*node.User, err
 
 // TODO this is basically a placeholder until we actually have a way of generating
 // the certificate for the node.
-func generateInitState(nodeConfig *config.NodeConfig) []byte {
+func generateInitState(nodeConfig *config.NodeConfig) ([]byte, error) {
 	nodeUUID := uuid.New().String()
 
 	rootCert := nodeConfig.RootUserCertB64()
 
-	return []byte(fmt.Sprintf(`{
-			"node_id": "%s",
-			"name": "My Habitat node",
-			"certificate": "placeholder",
-			"users": {
-				"%s": {
-					"id": "%s",
-					"username": "%s",
-					"certificate": "%s",
-					"app_installations": []
-				}
-			}
-		}`, nodeUUID, constants.RootUserID, constants.RootUserID, constants.RootUsername, rootCert))
+	emptyState, err := node.GetEmptyStateForVersion(node.LatestVersion)
+	if err != nil {
+		return nil, err
+	}
+
+	emptyState.NodeID = nodeUUID
+	emptyState.Users[constants.RootUserID] = &node.User{
+		ID:          constants.RootUserID,
+		Username:    constants.RootUsername,
+		Certificate: rootCert,
+	}
+
+	return json.Marshal(emptyState)
 }
