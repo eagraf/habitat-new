e9eeb059f611fc1cd3895e93a46254d2e87502ca
ethan <graf.ethan@gmail.com>
2024-06-16 23:42:00 -0400

    Embed frontend in Habitat node binary and serve it when not in dev mode

--- internal/frontend/server.go
+++ internal/frontend/server.go
@@ -1,23 +1,33 @@
 package frontend
 
 import (
+	"embed"
+	"io/fs"
 	"net/url"
 
 	"github.com/eagraf/habitat-new/internal/node/config"
 	"github.com/eagraf/habitat-new/internal/node/reverse_proxy"
 )
 
-func NewFrontendProxyRule(config *config.NodeConfig) reverse_proxy.RuleHandler {
+//go:embed build/*
+var frontendBuild embed.FS
+
+func NewFrontendProxyRule(config *config.NodeConfig) (reverse_proxy.RuleHandler, error) {
 	if config.FrontendDev() {
 		feDevServer, _ := url.Parse("http://habitat_frontend:8000/")
 		return &reverse_proxy.RedirectRule{
 			Matcher:         "/",
 			ForwardLocation: feDevServer,
-		}
+		}, nil
 	} else {
+
+		fSys, err := fs.Sub(frontendBuild, "build")
+		if err != nil {
+			return nil, err
+		}
 		return &reverse_proxy.FileServerRule{
 			Matcher: "/",
-			Path:    "frontend/out",
-		}
+			FS:      fSys,
+		}, nil
 	}
 }
