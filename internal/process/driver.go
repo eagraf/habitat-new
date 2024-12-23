9cae7168b3dd84807d2abaf994dc86569d4f50b2
Arushi Bandi <arushibandi@users.noreply.github.com>
2024-12-23 09:08:39 -0500

    more clarify

--- internal/process/driver.go
+++ internal/process/driver.go
@@ -7,3 +7,23 @@ type Driver interface {
 	StartProcess(*node.Process, *node.AppInstallation) (string, error)
 	StopProcess(extProcessID string) error
 }
+
+type noopDriver struct {
+	driverType string
+}
+
+func NewNoopDriver(driverType string) Driver {
+	return &noopDriver{driverType: driverType}
+}
+
+func (d *noopDriver) Type() string {
+	return d.driverType
+}
+
+func (d *noopDriver) StartProcess(*node.Process, *node.AppInstallation) (string, error) {
+	return "", nil
+}
+
+func (d *noopDriver) StopProcess(extProcessID string) error {
+	return nil
+}
--- internal/web/driver.go
+++ internal/web/driver.go
@@ -1,7 +1,6 @@
 package web
 
 import (
-	"github.com/eagraf/habitat-new/core/state/node"
 	"github.com/eagraf/habitat-new/internal/node/constants"
 	"github.com/eagraf/habitat-new/internal/process"
 )
@@ -9,24 +8,6 @@ import (
 // Currently the implementation is just no-ops because all we need is for the state machine
 // to mark the process as started or stopped, in order for files from the web bundle to be
 // served.
-type webDriver struct{}
-
-var _ process.Driver = &webDriver{}
-
 func NewDriver() process.Driver {
-	return &webDriver{}
-}
-
-func (d *webDriver) Type() string {
-	return constants.AppDriverWeb
-}
-
-func (d *webDriver) StartProcess(process *node.Process, app *node.AppInstallation) (string, error) {
-	// noop
-	return "", nil
-}
-
-func (d *webDriver) StopProcess(extProcessID string) error {
-	// noop
-	return nil
+	return process.NewNoopDriver(constants.AppDriverWeb)
 }
