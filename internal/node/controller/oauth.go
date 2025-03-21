f906af976ba711f21b0280d4114dd556f5a6f9c1
Ethan Graf <ethan@Beacon.local>
2025-03-20 22:09:11 -0400

    Get OAuth broker to work

--- internal/node/controller/oauth.go
+++ internal/node/controller/oauth.go
@@ -16,12 +16,12 @@ type HabitatOAuthProxy struct {
 func NewHabitatOAuthProxy(domain string, authPersister oauth.Persister) *HabitatOAuthProxy {
 
 	// In Habitat, the domain for this tailnet happens to be the same as the PDS URL.
-	//pdsURL := fmt.Sprintf("https://%s", domain)
-	pdsURL := "https://host.docker.internal:5001"
+	pdsURL := "http://host.docker.internal:5001"
 
 	oauthConfig := oauth.Config{
-		Protocol:     "https",
-		Host:         domain,
+		Protocol: "https",
+		Host:     domain,
+		// TODO @eagraf we need a flow for generating this
 		SecretJWK:    `{"use":"sig","kty":"EC","kid":"demo-1737595555","crv":"P-256","alg":"ES256","x":"1hF99bxkcHj-aTWnEYzPJvrb8p0d2mfr7OI1NsTYHV4","y":"QWXmL4UwsdtLnKNJKppnr3RPf6NsZx7-IGWRuuySJh4","d":"pcSEJr8TirUbxaCbQ_L17x_00_SDvB1jZpZmNsRra_Y"}`,
 		PDSURL:       pdsURL,
 		EndpointPath: "/habitat/oauth",
@@ -58,10 +58,8 @@ func (h *HabitatOAuthProxy) logoutHandler(w http.ResponseWriter, r *http.Request
 }
 
 func (h *HabitatOAuthProxy) xrpcBrokerHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("TOO BROKE")
-	a := 1
-	fmt.Println(a)
-	broker := oauth.NewTokenBroker(h.persister, h.oauthConfig.SecretJWK, h.oauthConfig.SecretJWK, h.oauthConfig.Host)
+	externalURL := fmt.Sprintf("https://%s", r.Host)
+	broker := oauth.NewTokenBroker(h.persister, h.oauthConfig.Host, h.oauthConfig.PDSURL, externalURL)
 	broker.Endpoint().ServeHTTP(w, r)
 }
 
