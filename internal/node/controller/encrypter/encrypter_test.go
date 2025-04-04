9318f05fb155023130e2f9519d0ae0080f2d7192
Arushi Bandi <arushibandi@users.noreply.github.com>
2025-04-03 22:28:15 -0700

    added gcm-siv

--- internal/node/controller/encrypter/encrypter_test.go
+++ internal/node/controller/encrypter/encrypter_test.go
@@ -3,6 +3,9 @@ package encrypter
 import (
 	"crypto/rand"
 	"encoding/hex"
+	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func randomKey(numBytes int) string {
@@ -14,28 +17,17 @@ func randomKey(numBytes int) string {
 	return hex.EncodeToString(bytes) //encode key in bytes to string and keep as secret, put in a vault
 }
 
-/*
 func TestAesEncrypter(t *testing.T) {
-	var err error
-	ciphers := make([]cipher.Block, 5)
-	for i := range 5 {
-		ciphers[i], err = aes.NewCipher([]byte(randomKey(16)))
-		require.NoError(t, err)
-	}
-	e := NewFromKeys(ciphers)
+	e, err := NewFromKey([]byte(randomKey(16)))
+	require.NoError(t, err)
 
 	rkey := "my-rkey"
 	data := []byte("this is my data lalalala")
 
 	// Make sure decrypt(encrypted) == encrypt(decrypted)
-	encrypted := e.Encrypt(rkey, data)
-	require.Equal(t, data, e.Decrypt(rkey, encrypted))
-
-	// Make sure the right cipher was used
-	i := e.cipherIndexFromRkey(rkey)
-	cipher := e.ciphers[i]
-	var src []byte
-	cipher.Decrypt(encrypted, src)
-	require.Equal(t, data, src)
+	enc, err := e.Encrypt(rkey, data)
+	require.NoError(t, err)
+	dec, err := e.Decrypt(rkey, enc)
+	require.NoError(t, err)
+	require.Equal(t, dec, data)
 }
-*/
