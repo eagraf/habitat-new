f7f46894ca2629662420a8229df889dc3523fdbb
Arushi Bandi <arushibandi@users.noreply.github.com>
2025-04-04 15:41:48 -0700

    my test works woohoo

--- internal/node/controller/private_data_test.go
+++ internal/node/controller/private_data_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -18,29 +17,36 @@ import (
 	"github.com/eagraf/habitat-new/internal/process"
 	"github.com/ipfs/go-cid"
 	"github.com/stretchr/testify/require"
-	cbg "github.com/whyrusleeping/cbor-gen"
 )
 
-type testCborMarshaler struct {
-	val []byte
+type mockTripper struct {
+	t     *testing.T
+	curr  int
+	reqs  []*http.Request
+	resps []*http.Response
+	errs  []error
 }
 
-func (t testCborMarshaler) MarshalCBOR(io.Writer) error {
-	return nil
+func (m *mockTripper) RoundTrip(r *http.Request) (*http.Response, error) {
+	require.Equal(m.t, m.reqs[m.curr], r)
+	resp := m.resps[m.curr]
+	err := m.errs[m.curr]
+	m.curr++
+	return resp, err
 }
-
-var _ cbg.CBORMarshaler = testCborMarshaler{}
 
 // TODO: An integration test with PDS running + real encryption
 // This mocks out the PDS and uses a no-op encrypter
 func TestControllerPrivateDataPutGet(t *testing.T) {
 	ctx := context.Background()
+	encrypter, err := encrypter.NewFromKey([]byte(encrypter.TestOnlyNewRandomKey()))
+	require.NoError(t, err)
 
 	type req struct {
-		url     string
-		req     []byte
-		resp    []byte
-		respErr *error
+		url   string
+		req   []byte
+		resp  []byte
+		isErr bool
 	}
 
 	// The val the caller is trying to put
@@ -65,24 +71,29 @@ func TestControllerPrivateDataPutGet(t *testing.T) {
 	}
 	resp2, err := json.Marshal(put2)
 	require.NoError(t, err)
-	/*
 
-		blob := &util.BlobSchema{
-			LexiconTypeID: "my.fake.collection",
-			Ref:           util.LexLink(cid.MustParse(blobCid)),
-		}
-		lexval := &util.LexiconTypeDecoder{
-			Val: blob,
-		}
-			get3 := atproto.RepoGetRecord_Output{
-				Cid:   &blobCid,
-				Uri:   testUri,
-				Value: lexval,
-			}
+	xrpcErr := xrpc.XRPCError{
+		ErrStr:  "Could not locate record",
+		Message: "Could not locate record",
+	}
+	resp3, err := json.Marshal(xrpcErr)
+	require.NoError(t, err)
 
-			//resp3, err := json.Marshal(get3)
-			//require.NoError(t, err)
-	*/
+	resp4 := []byte(fmt.Sprintf(`{
+		"cid": "%s", 
+		"uri": "%s", 
+		"value": {
+			"$type": "com.habitat.encryptedRecord",
+			"cid": "%s"
+		}
+	}`, encRecordCid, testUri, blobCid))
+
+	marshalledVal, err := json.Marshal(val)
+	require.NoError(t, err)
+	encrypted, err := encrypter.Encrypt("my-rkey", marshalledVal)
+	require.NoError(t, err)
+	resp5 := encrypted
+
 	reqOrder := []req{
 		{
 			url:  "/xrpc/com.atproto.repo.uploadBlob",
@@ -93,9 +104,17 @@ func TestControllerPrivateDataPutGet(t *testing.T) {
 			resp: resp2,
 		},
 		{
-			url:     "/xrpc/com.atproto.repo.getRecord?cid=&collection=my.fake.collection&repo=my-did&rkey=my-rkey",
-			resp:    []byte("Could not locate record"),
-			respErr: &(fmt.Errorf("Could not locate record")),
+			url:   "/xrpc/com.atproto.repo.getRecord?cid=&collection=my.fake.collection&repo=my-did&rkey=my-rkey",
+			resp:  resp3,
+			isErr: true,
+		},
+		{
+			url:  "/xrpc/com.atproto.repo.getRecord?cid=&collection=com.habitat.encryptedRecord&repo=my-did&rkey=enc%3Amy.fake.collection%3Amy-rkey",
+			resp: resp4,
+		},
+		{
+			url:  fmt.Sprintf("/xrpc/com.atproto.sync.getBlob?cid=%s&did=my-did", blobCid),
+			resp: resp5,
 		},
 	}
 	curr := 0
@@ -103,6 +122,10 @@ func TestControllerPrivateDataPutGet(t *testing.T) {
 	mockPDS := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		expReq := reqOrder[curr]
 		require.Equal(t, expReq.url, r.URL.String())
+
+		if expReq.isErr {
+			w.WriteHeader(http.StatusForbidden)
+		}
 		w.Write(expReq.resp)
 		curr += 1
 	}))
@@ -111,8 +134,6 @@ func TestControllerPrivateDataPutGet(t *testing.T) {
 	mockDriver := newMockDriver(node.DriverTypeDocker)
 	pm := process.NewProcessManager([]process.Driver{mockDriver})
 
-	encrypter, err := encrypter.NewFromKey([]byte(encrypter.TestOnlyNewRandomKey()))
-	require.NoError(t, err)
 	ctrl, err := NewController2(
 		context.Background(),
 		pm,
@@ -127,7 +148,6 @@ func TestControllerPrivateDataPutGet(t *testing.T) {
 		},
 		encrypter,
 	)
-	require.NoError(t, err)
 
 	// putRecord with encryption
 	coll := "my.fake.collection"
@@ -143,25 +163,8 @@ func TestControllerPrivateDataPutGet(t *testing.T) {
 
 	got, err := ctrl.getRecord(ctx, "", coll, "my-did", "my-rkey")
 	require.NoError(t, err)
-	require.Equal(t, *got.Cid, blobCid)
+	require.Equal(t, *got.Cid, encRecordCid)
 	require.Equal(t, got.Uri, testUri)
 	bytes, err := got.Value.MarshalJSON()
-	require.NoError(t, err)
-	fmt.Println(string(bytes))
-
-	/*
-		// TODO: putRecord no encryption
-		out, err = ctrl.putRecord(ctx, &agnostic.RepoPutRecord_Input{
-			Collection: coll,
-			Record:     val,
-			Repo:       "my-did",
-			Rkey:       "my-rkey",
-		}, false)
-		require.NoError(t, err)
-
-		resp, err = ctrl.getRecord(ctx, out.Cid, coll, "my-did", "my-rkey")
-		require.NoError(t, err)
-		require.Equal(t, resp.Cid, out.Cid)
-		require.Equal(t, val, resp.Value)
-	*/
+	require.Equal(t, bytes, marshalledVal)
 }
