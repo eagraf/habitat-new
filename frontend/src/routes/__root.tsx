ec6186b057f9b787657e5d61d43640e89eae71fe
Sashank Gogula <sashankg@gmail.com>
2025-04-15 18:26:38 -0700

    use @atproto/oauth-client-browser

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,9 +1,19 @@
 import { AuthProvider } from '@/components/authContext'
 import Header from '@/components/header'
+import { oauthClient } from '@/lib/oauthClient'
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
 export const Route = createRootRoute({
+  async loader() {
+    try {
+      const result = await oauthClient.init()
+      console.log(result)
+    }
+    catch (e) {
+      console.log(e)
+    }
+  },
   component: () => (
     <AuthProvider>
       <Header />
