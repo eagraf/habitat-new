6ff1ea74a774f7fb91576f944db0ffd67dd4b982
Sashank Gogula <sashankg@gmail.com>
2025-04-15 23:00:16 -0700

    replace auth context

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,14 +1,52 @@
-import { AuthProvider } from '@/components/authContext'
 import Header from '@/components/header'
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import type { BrowserOAuthClient, OAuthSession } from '@atproto/oauth-client-browser'
+import { useMutation, type QueryClient } from '@tanstack/react-query'
+import { Outlet, createRootRouteWithContext, useRouter } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
-export const Route = createRootRoute({
-  component: () => (
-    <AuthProvider>
-      <Header />
-      <Outlet />
-      <TanStackRouterDevtools />
-    </AuthProvider>
-  ),
+interface RouterContext {
+  queryClient: QueryClient
+  oauthClient: BrowserOAuthClient
+  authSession: OAuthSession | undefined
+}
+
+export const Route = createRootRouteWithContext<RouterContext>()({
+  async beforeLoad({ context }) {
+    const result = await context.oauthClient.init()
+    return {
+      authSession: result?.session
+    }
+  },
+  async loader({ context }) {
+    if (!context.authSession) {
+      return {}
+    }
+    const identityReq = new URL('/xrpc/com.atproto.repo.describeRepo', window.location.origin)
+    identityReq.searchParams.set('repo', context.authSession.did)
+
+    const response = await context.authSession.fetchHandler(identityReq.toString())
+    const details = await response.json() as { handle: string }
+    return {
+      handle: details.handle,
+    }
+  },
+  component() {
+    const { invalidate } = useRouter()
+    const { authSession, oauthClient } = Route.useRouteContext()
+    const { handle } = Route.useLoaderData();
+    const { mutate: logout } = useMutation({
+      async mutationFn() {
+        if (!authSession) return
+        oauthClient.revoke(authSession.sub)
+        invalidate()
+      }
+    })
+    return (
+      <>
+        <Header isAuthenticated={!!authSession} handle={handle} onLogout={logout} />
+        <Outlet />
+        <TanStackRouterDevtools />
+      </>
+    )
+  },
 })
