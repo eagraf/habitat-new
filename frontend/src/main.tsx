20cdbc09fdfd30a6976a527331d641eb19ef555d
Sashank Gogula <sashankg@gmail.com>
2025-05-17 15:15:51 -0700

    move oauthClient to main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,7 +7,19 @@ import { routeTree } from './routeTree.gen'
 
 import reportWebVitals from './reportWebVitals.ts'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { oauthClient } from './lib/oauthClient.ts'
+import { BrowserOAuthClient } from '@atproto/oauth-client-browser'
+
+
+export const oauthClient = new BrowserOAuthClient({
+  handleResolver: 'https://bsky.social',
+  clientMetadata: {
+    client_id: 'http://localhost?scope=atproto%20transition%3Ageneric',
+    redirect_uris: ['http://127.0.0.1:3001/'],
+    scope: 'atproto transition:generic',
+    token_endpoint_auth_method: 'none'
+  },
+  allowHttp: true
+})
 
 const queryClient = new QueryClient();
 
