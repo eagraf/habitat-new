ad001b78d5b0450ce338f86c81f3a136bd92d289
Ethan Graf <ethan@Beacon.local>
2025-07-02 22:47:44 -0400

    Refactor jwks

--- internal/auth/dpop.go
+++ internal/auth/dpop.go
@@ -11,7 +11,6 @@ import (
 	"io"
 	"net/http"
 	"strings"
-	"time"
 
 	"github.com/bluesky-social/indigo/atproto/identity"
 	jose "github.com/go-jose/go-jose/v3"
@@ -46,42 +45,6 @@ type dpopClaims struct {
 
 	// the `nonce` claim. See https://datatracker.ietf.org/doc/html/draft-ietf-oauth-dpop#section-4.2
 	Nonce string `json:"nonce,omitempty"`
-}
-
-// DpopJWKBuilder is a function that builds a DPoP JWT for a given request type.
-type DpopJWKBuilder func(req *http.Request, signer jose.Signer, session *sessions.Session, nonce string) (string, error)
-
-func getPDSJWKBuilder(htu string) DpopJWKBuilder {
-	return func(req *http.Request, signer jose.Signer, session *sessions.Session, nonce string) (string, error) {
-		accessTokenHash, _ := session.Values[cAccessTokenHashSessionKey].(string)
-
-		return jwt.Signed(signer).Claims(&dpopClaims{
-			Claims: jwt.Claims{
-				ID:       generateNonce(),
-				IssuedAt: jwt.NewNumericDate(time.Now()),
-			},
-			Method:          req.Method,
-			URL:             htu,
-			Nonce:           nonce,
-			AccessTokenHash: accessTokenHash,
-		}).CompactSerialize()
-	}
-}
-
-func getAuthServerJWKBuilder() DpopJWKBuilder {
-	return func(req *http.Request, signer jose.Signer, session *sessions.Session, nonce string) (string, error) {
-		htu := req.URL.String()
-
-		return jwt.Signed(signer).Claims(&dpopClaims{
-			Claims: jwt.Claims{
-				ID:       generateNonce(),
-				IssuedAt: jwt.NewNumericDate(time.Now()),
-			},
-			Method: req.Method,
-			URL:    htu,
-			Nonce:  nonce,
-		}).CompactSerialize()
-	}
 }
 
 type DpopHttpClient struct {
---
+++ internal/auth/jwks.go
@@ -0,0 +1,46 @@
+package auth
+
+import (
+	"net/http"
+	"time"
+
+	jose "github.com/go-jose/go-jose/v3"
+	"github.com/go-jose/go-jose/v3/jwt"
+	"github.com/gorilla/sessions"
+)
+
+// DpopJWKBuilder is a function that builds a DPoP JWT for a given request type.
+type DpopJWKBuilder func(req *http.Request, signer jose.Signer, session *sessions.Session, nonce string) (string, error)
+
+func getPDSJWKBuilder(htu string) DpopJWKBuilder {
+	return func(req *http.Request, signer jose.Signer, session *sessions.Session, nonce string) (string, error) {
+		accessTokenHash, _ := session.Values[cAccessTokenHashSessionKey].(string)
+
+		return jwt.Signed(signer).Claims(&dpopClaims{
+			Claims: jwt.Claims{
+				ID:       generateNonce(),
+				IssuedAt: jwt.NewNumericDate(time.Now()),
+			},
+			Method:          req.Method,
+			URL:             htu,
+			Nonce:           nonce,
+			AccessTokenHash: accessTokenHash,
+		}).CompactSerialize()
+	}
+}
+
+func getAuthServerJWKBuilder() DpopJWKBuilder {
+	return func(req *http.Request, signer jose.Signer, session *sessions.Session, nonce string) (string, error) {
+		htu := req.URL.String()
+
+		return jwt.Signed(signer).Claims(&dpopClaims{
+			Claims: jwt.Claims{
+				ID:       generateNonce(),
+				IssuedAt: jwt.NewNumericDate(time.Now()),
+			},
+			Method: req.Method,
+			URL:    htu,
+			Nonce:  nonce,
+		}).CompactSerialize()
+	}
+}
