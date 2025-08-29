85aff24b6e73a95d9a7c9c25fc9266bc11d71513
Arushi Bandi <arushibandi@users.noreply.github.com>
2025-08-29 09:08:29 -0700

    remove more unused

--- internal/node/hdb/client.go
+++
@@ -1,6 +0,0 @@
-package hdb
-
-type Client interface {
-	ProposeTransitions(transitions []Transition) (*JSONState, error)
-	Bytes() []byte
-}
--- internal/node/hdb/error.go
+++ internal/node/hdb/error.go
@@ -2,19 +2,6 @@ package hdb
 
 import "errors"
 
-type DatabaseNotFoundError struct {
-	DatabaseName string
-	DatabaseID   string
-}
-
-func (e *DatabaseNotFoundError) Error() string {
-	if e.DatabaseName != "" {
-		return "Database with name " + e.DatabaseName + " not found"
-	} else {
-		return "Database with id " + e.DatabaseID + " not found"
-	}
-}
-
 var (
 	DatabaseAlreadyExistsError = errors.New("database already exists")
 )
