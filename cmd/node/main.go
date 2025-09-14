ecd7988f4914a4ec3c07b26793d39f0c4049b331
Arushi Bandi <arushibandi@users.noreply.github.com>
2025-09-14 16:34:33 -0700

    privi persists to sqlite db

--- cmd/node/main.go
+++ cmd/node/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"errors"
 	"fmt"
 	"net"
@@ -167,52 +168,7 @@ func main() {
 		routes = append(routes, appstore.NewAvailableAppsRoute(nodeConfig.HabitatPath()))
 	}
 
-	// TODO: read from persisted state about permissions.
-	policiesDirPath := nodeConfig.PermissionPolicyFilesDir()
-	policiesDir, err := os.ReadDir(policiesDirPath)
-	if errors.Is(err, os.ErrNotExist) {
-		log.Info().Msgf("Creating a policies dir path at %s", policiesDirPath)
-		err := os.Mkdir(policiesDirPath, 0700)
-		if err != nil {
-			log.Fatal().Err(err).Msgf("error creating permission policies dir at %s", policiesDirPath)
-		}
-	} else if err != nil {
-		log.Fatal().Err(err).Msg("error reading from permission policies dir")
-	}
-
-	perms := make(map[syntax.DID]permissions.Store, len(policiesDir))
-
-	for _, file := range policiesDir {
-		// Convention is to store policies for a given did in a file called <did>_policies.csv
-		name := file.Name()
-		spl := strings.Split(file.Name(), "_")
-		if len(spl) < 2 {
-			log.Fatal().Err(err).Msgf("invalid naming for permission policy file: found %s, expected %s", file.Name(), "<did>_policies.csv")
-		}
-		did := spl[0]
-		adapter := fileadapter.NewAdapter(filepath.Join(policiesDirPath, name))
-		store, err := permissions.NewStore(adapter, true)
-		if err != nil {
-			log.Fatal().Err(err).Msgf("unable to initialize permissions store for user %s", did)
-		}
-		perms[syntax.DID(did)] = store
-	}
-
-	// FOR DEMO PURPOSES ONLY
-	sashankDID := "did:plc:v3amhno5wvyfams6aioqqj66"
-	_, ok := perms[syntax.DID(sashankDID)]
-	if !ok {
-		perms[syntax.DID(sashankDID)] = permissions.NewDummyStore()
-	}
-	err = perms[syntax.DID(sashankDID)].AddLexiconReadPermission(sashankDID, "com.habitat.test")
-	if err != nil {
-		log.Err(err).Msgf("error adding test lexicon for sashank demo")
-	}
-
-	// Add privy routes
-	priviServer := privi.NewServer(
-		perms,
-	)
+	priviServer := setupPrivi(nodeConfig)
 	routes = append(routes, priviServer.GetRoutes()...)
 
 	router := api.NewRouter(routes, logger)
@@ -258,6 +214,87 @@ func main() {
 		log.Err(err).Msg("received error on eg.Wait()")
 	}
 	log.Info().Msg("Finished!")
+}
+
+func setupPrivi(nodeConfig *config.NodeConfig) *privi.Server {
+	policiesDirPath := nodeConfig.PermissionPolicyFilesDir()
+	policiesDir, err := os.ReadDir(policiesDirPath)
+	if errors.Is(err, os.ErrNotExist) {
+		log.Info().Msgf("Creating a policies dir path at %s", policiesDirPath)
+		err := os.Mkdir(policiesDirPath, 0700)
+		if err != nil {
+			log.Fatal().Err(err).Msgf("error creating permission policies dir at %s", policiesDirPath)
+		}
+	} else if err != nil {
+		log.Fatal().Err(err).Msg("error reading from permission policies dir")
+	}
+
+	// TODO: move all the privi stuff into a helper
+	perms := make(map[syntax.DID]permissions.Store, len(policiesDir))
+
+	for _, file := range policiesDir {
+		// Convention is to store policies for a given did in a file called <did>_policies.csv
+		name := file.Name()
+		spl := strings.Split(file.Name(), "_")
+		if len(spl) < 2 {
+			log.Fatal().Err(err).Msgf("invalid naming for permission policy file: found %s, expected %s", file.Name(), "<did>_policies.csv")
+		}
+		did := spl[0]
+		adapter := fileadapter.NewAdapter(filepath.Join(policiesDirPath, name))
+		store, err := permissions.NewStore(adapter, true)
+		if err != nil {
+			log.Fatal().Err(err).Msgf("unable to initialize permissions store for user %s", did)
+		}
+		perms[syntax.DID(did)] = store
+	}
+
+	// FOR DEMO PURPOSES ONLY
+	sashankDID := "did:plc:v3amhno5wvyfams6aioqqj66"
+	_, ok := perms[syntax.DID(sashankDID)]
+	if !ok {
+		perms[syntax.DID(sashankDID)] = permissions.NewDummyStore()
+	}
+	err = perms[syntax.DID(sashankDID)].AddLexiconReadPermission(sashankDID, "com.habitat.test")
+	if err != nil {
+		log.Err(err).Msgf("error adding test lexicon for sashank demo")
+	}
+
+	// Create database file if it does not exist
+	priviRepoPath := nodeConfig.PriviRepoFile()
+	_, err = os.Stat(priviRepoPath)
+	if errors.Is(err, os.ErrNotExist) {
+		_, err := os.Create(priviRepoPath)
+		if err != nil {
+			log.Err(err).Msgf("unable to create privi repo file at ", priviRepoPath)
+		}
+	} else if err != nil {
+		log.Err(err).Msgf("error finding privi repo file")
+
+	}
+
+	priviDB, err := sql.Open("sqlite3", priviRepoPath)
+	if err != nil {
+		log.Fatal().Err(err).Msg("unable to open sqlite file backing privi server")
+	}
+	createTableSQL := `
+	CREATE TABLE IF NOT EXISTS records (
+		did TEXT,
+		rkey TEXT NOT NULL UNIQUE,
+		record TEXT,
+		PRIMARY KEY(did, rkey)
+	);`
+	_, err = priviDB.Exec(createTableSQL)
+	if err != nil {
+		log.Fatal().Err(err).Msg("unable to setup privi sqlite db")
+	}
+	defer priviDB.Close()
+
+	// Add privy routes
+	priviServer := privi.NewServer(
+		perms,
+		privi.NewSQLiteRepo(priviDB),
+	)
+	return priviServer
 }
 
 func generateDefaultReverseProxyRules(config *config.NodeConfig) ([]*reverse_proxy.Rule, error) {
---
+++ internal/privi/repo.go
@@ -0,0 +1,153 @@
+package privi
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+
+	_ "github.com/mattn/go-sqlite3"
+
+	"github.com/bluesky-social/indigo/atproto/data"
+	"github.com/bluesky-social/indigo/atproto/syntax"
+)
+
+// Persist private data within repos that mirror public repos.
+// A repo implements two methods: putRecord and getRecord.
+// In the future, it is possible to implement sync endpoints and other methods.
+
+type repo interface {
+	putRecord(did string, rkey string, rec record, validate *bool) error
+	getRecord(did string, rkey string) (record, error)
+}
+
+// Lexicon NSID -> records for that lexicon.
+// A record is stored as raw bytes and keyed by its record key (rkey).
+//
+// TODO: the internal store should be an MST for portability / compatiblity with conventional atproto  methods.
+type inMemoryRepo map[syntax.DID]map[recordKey]record
+
+// inMemoryRepo implements repo
+var _ repo = &inMemoryRepo{}
+
+func newInMemoryRepo() inMemoryRepo {
+	return make(inMemoryRepo)
+}
+
+// putRecord puts a record for the given rkey into the repo no matter what; if a record always exists, it is overwritten.
+func (r inMemoryRepo) putRecord(did string, rkey string, rec record, validate *bool) error {
+	if validate != nil && *validate {
+		err := data.Validate(rec)
+		if err != nil {
+			return err
+		}
+	}
+
+	coll, ok := r[syntax.DID(did)]
+	if !ok {
+		coll = make(map[recordKey]record)
+		r[syntax.DID(did)] = coll
+	}
+
+	// Always put (even if something exists).
+	coll[recordKey(rkey)] = rec
+	return nil
+}
+
+func (r inMemoryRepo) getRecord(did string, rkey string) (record, error) {
+	coll, ok := r[syntax.DID(did)]
+	if !ok {
+		return nil, ErrRecordNotFound
+	}
+
+	record, ok := coll[recordKey(rkey)]
+	return record, nil
+}
+
+// A sqlite-backed repo per user contains the following two columns:
+// [did, record key, record value]
+// For now, store all records in the same database. Eventually, this should be broken up into
+// per-user databases or per-user MST repos.
+
+type sqliteRepo struct {
+	db *sql.DB
+}
+
+var _ repo = &sqliteRepo{}
+
+// Shape of a row in the sqlite table
+type row struct {
+	did  string
+	rkey string
+	rec  string
+}
+
+// TODO: create table etc.
+func NewSQLiteRepo(db *sql.DB) repo {
+	return &sqliteRepo{
+		db: db,
+	}
+}
+
+// putRecord puts a record for the given rkey into the repo no matter what; if a record always exists, it is overwritten.
+func (r *sqliteRepo) putRecord(did string, rkey string, rec record, validate *bool) error {
+	if validate != nil && *validate {
+		err := data.Validate(rec)
+		if err != nil {
+			return err
+		}
+	}
+
+	bytes, err := json.Marshal(rec)
+	if err != nil {
+		return err
+	}
+	// Always put (even if something exists).
+	_, err = r.db.Exec("insert into records(did, rkey, record) values(?, ?, ?);", did, rkey, string(bytes))
+	return err
+}
+
+var (
+	ErrRecordNotFound       = fmt.Errorf("record not found")
+	ErrMultipleRecordsFound = fmt.Errorf("multiple records found for desired query")
+)
+
+func (r *sqliteRepo) getRecord(did string, rkey string) (record, error) {
+	rows, err := r.db.Query("select * from records where rkey = ? and did = ?", rkey, did)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	numRows := 0
+	var row row
+
+	for rows.Next() {
+		numRows++
+		err = rows.Scan(&row.did, &row.rkey, &row.rec)
+		if err != nil {
+			return nil, err
+		}
+		break
+	}
+
+	if numRows == 0 {
+		return nil, ErrRecordNotFound
+	} else if rows.Next() {
+		return nil, ErrMultipleRecordsFound
+	}
+
+	var record map[string]any
+	err = json.Unmarshal([]byte(row.rec), &record)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: return ErrorRecordNotFound somewhere
+	return record, nil
+}
+
+// Return the did from the path name
+// This is not that secure because someone can easily change the file name. Probably need a better way to store this.
+func didFromPath(path string) string {
+	return path
+}
