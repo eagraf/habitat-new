90e7ee807bf8b4d7227f5b94ebb22b2c14c6672e
Sashank Gogula <sashankg@gmail.com>
2025-10-20 20:06:54 -0700

    fix dummy directory

--- internal/auth/dummy_directory.go
+++ internal/auth/dummy_directory.go
@@ -18,19 +18,28 @@ func NewDummyDirectory(url string) *DummyDirectory {
 	}
 }
 
-func (d *DummyDirectory) LookupHandle(ctx context.Context, handle syntax.Handle) (*identity.Identity, error) {
+func (d *DummyDirectory) LookupHandle(
+	ctx context.Context,
+	handle syntax.Handle,
+) (*identity.Identity, error) {
 	return nil, fmt.Errorf("unimplemented")
 }
 
-func (d *DummyDirectory) LookupDID(ctx context.Context, did syntax.DID) (*identity.Identity, error) {
+func (d *DummyDirectory) LookupDID(
+	ctx context.Context,
+	did syntax.DID,
+) (*identity.Identity, error) {
 	return nil, fmt.Errorf("unimplemented")
 }
 
-func (d *DummyDirectory) Lookup(ctx context.Context, atid syntax.AtIdentifier) (*identity.Identity, error) {
+func (d *DummyDirectory) Lookup(
+	ctx context.Context,
+	atid syntax.AtIdentifier,
+) (*identity.Identity, error) {
 	did := atid.String()
 	return &identity.Identity{
 		DID: syntax.DID(did),
-		Services: map[string]identity.Service{
+		Services: map[string]identity.ServiceEndpoint{
 			"atproto_pds": {
 				URL: d.URL,
 			},
