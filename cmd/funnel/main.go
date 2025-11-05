f0fdb2fba996727eebdfef128be3663fa19e5265
Sashank Gogula <sashankg@gmail.com>
2025-11-04 18:51:20 -0800

    restore funnel

--- cmd/funnel/main.go
+++ cmd/funnel/main.go
@@ -1,19 +1,14 @@
 package main
 
 import (
-	"context"
-	"flag"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
 	"github.com/eagraf/habitat-new/internal/node/config"
-	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
+	"github.com/eagraf/habitat-new/internal/node/logging"
+	"github.com/eagraf/habitat-new/util"
 	"tailscale.com/tsnet"
 )
 
@@ -22,73 +17,32 @@ func main() {
 	port := os.Args[1]
 	hostName := os.Args[2]
 
-	zerolog.SetGlobalLevel(zerolog.InfoLevel)
-	flag.BoolFunc("debug", "", func(_ string) error {
-		zerolog.SetGlobalLevel(zerolog.DebugLevel)
-		return nil
-	})
-	flag.Parse()
+	logger := logging.NewLogger()
 
 	nodeConfig, err := config.NewNodeConfig()
 	if err != nil {
-		log.Fatal().Err(err).Msg("error loading node config")
+		logger.Fatal().Err(err).Msg("error loading node config")
 	}
 
 	server := &tsnet.Server{
 		Hostname: hostName,
 		AuthKey:  nodeConfig.TailscaleAuthkey(),
-		Logf: func(format string, args ...any) {
-			log.Debug().Msgf(format, args...)
-		},
 	}
+	defer util.Close(server)
 
 	ln, err := server.ListenFunnel("tcp", ":443")
 	if err != nil {
-		log.Fatal().Err(err).Msg("unable to listen")
+		logger.Fatal().Err(err).Msg("unable to listen")
 	}
 
 	localUrl, err := url.Parse("http://localhost:" + port)
 	if err != nil {
-		log.Fatal().Err(err).Msg("unable to parse url")
+		logger.Fatal().Err(err).Msg("unable to parse url")
 	}
 	proxy := httputil.NewSingleHostReverseProxy(localUrl)
 
-	// Create HTTP server
-	httpServer := &http.Server{
-		Handler: proxy,
+	err = http.Serve(ln, proxy)
+	if err != nil {
+		logger.Fatal().Err(err).Msg("unable to serve")
 	}
-
-	// Setup signal handling for graceful shutdown
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
-
-	// Run server in goroutine
-	serverErr := make(chan error, 1)
-	go func() {
-		log.Info().Msg("starting funnel server")
-		serverErr <- httpServer.Serve(ln)
-	}()
-
-	// Wait for interrupt signal or server error
-	select {
-	case err := <-serverErr:
-		if err != nil && err != http.ErrServerClosed {
-			log.Fatal().Err(err).Msg("server error")
-		}
-	case sig := <-sigChan:
-		log.Info().Msgf("received signal %v, shutting down gracefully", sig)
-	}
-
-	// Graceful shutdown with timeout
-	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	if err := httpServer.Shutdown(shutdownCtx); err != nil {
-		log.Warn().Err(err).Msg("error during HTTP server shutdown")
-	}
-	// Close the tsnet server
-	if err := server.Close(); err != nil {
-		log.Warn().Err(err).Msg("error closing tsnet server")
-	}
-	log.Info().Msg("server stopped - exiting")
 }
