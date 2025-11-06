f8fb243f5a52658315b513e6aceb2356407fb4a1
Sashank Gogula <sashankg@gmail.com>
2025-11-05 19:36:40 -0800

    [Frontend] Setup github pages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import {
+  RouterProvider,
+  createHashHistory,
+  createRouter,
+} from "@tanstack/react-router";
 import clientMetadata from "../client-metadata";
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
@@ -34,6 +38,7 @@ const router = createRouter({
   scrollRestoration: true,
   defaultStructuralSharing: true,
   defaultPreloadStaleTime: 0,
+  history: __HASH_ROUTING__ ? createHashHistory() : undefined,
 });
 
 // Register the router instance for type safety
